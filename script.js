8cd41589040ff80f211c6c2d4800e95e97aa06e9
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-10-11 01:57:26 +0300

    Adjusts navbar to work responsive

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 const backgroundColor = document.getElementsByClassName('hero-nav')[0];
 const hamburgerColor = document.getElementsByClassName('bar')[0];
-const heroColor = document.getElementsByClassName('hero-logo')[0];
+const heroLogoColor = document.getElementsByClassName('hero-logo')[0];
+const heroColor = document.getElementsByClassName('hero')[0];
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
   toggleButton.classList.toggle('active');
   backgroundColor.classList.toggle('active');
   hamburgerColor.classList.toggle('active');
+  heroLogoColor.classList.toggle('active');
   heroColor.classList.toggle('active');
 });
 
@@ -18,6 +20,7 @@ document.querySelectorAll('.navbar-links').forEach((n) => {
     toggleButton.classList.remove('active');
     backgroundColor.classList.remove('active');
     hamburgerColor.classList.remove('active');
+    heroLogoColor.classList.remove('active');
     heroColor.classList.remove('active');
   });
 });
