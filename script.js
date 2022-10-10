909feac37115d19caf29d2beafe235bf1f869a94
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-10-10 23:26:33 +0300

    Converts double quotes to single quotes

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const backgroundColor = document.getElementsByClassName("hero-nav")[0];
-const hamburgerColor = document.getElementsByClassName("bar")[0];
-const heroColor = document.getElementsByClassName("hero-logo")[0];
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const backgroundColor = document.getElementsByClassName('hero-nav')[0];
+const hamburgerColor = document.getElementsByClassName('bar')[0];
+const heroColor = document.getElementsByClassName('hero-logo')[0];
 
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-  toggleButton.classList.toggle("active");
-  backgroundColor.classList.toggle("active");
-  hamburgerColor.classList.toggle("active");
-  heroColor.classList.toggle("active");
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  toggleButton.classList.toggle('active');
+  backgroundColor.classList.toggle('active');
+  hamburgerColor.classList.toggle('active');
+  heroColor.classList.toggle('active');
 });
 
-document.querySelectorAll(".navbar-links").forEach((n) => {
-  n.addEventListener("click", () => {
-    navbarLinks.classList.remove("active");
-    toggleButton.classList.remove("active");
-    backgroundColor.classList.remove("active");
-    hamburgerColor.classList.remove("active");
-    heroColor.classList.remove("active");
+document.querySelectorAll('.navbar-links').forEach((n) => {
+  n.addEventListener('click', () => {
+    navbarLinks.classList.remove('active');
+    toggleButton.classList.remove('active');
+    backgroundColor.classList.remove('active');
+    hamburgerColor.classList.remove('active');
+    heroColor.classList.remove('active');
   });
 });
