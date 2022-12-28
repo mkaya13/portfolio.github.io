e13f3ec62b2b2b9f2f8245e83e27716c5a1dd9d0
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2022-12-29 01:05:45 +0300

    Updates portfolio

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const emailLetterCheck = document.querySelector('#span-upper-case');
 
 const cardContainer = [
   {
-    title: 'Leaderboard Ranking',
+    title: 'Awesome Series',
     SubTitle: ['MICROVERSE', 'Web Development', 2022],
-    text: 'Leaderboard project lets you create an API endpoint with a POST request to an API with node.js, populate the created API endpoint with a post request through user interface form, and send a get request with refresh button to see the data stored in the API.',
-    img: 'leaderboard.png',
-    languages: ['html', 'css', 'js'],
-    seeLive: 'https://mkaya13.github.io/leaderboard-project/dist/index.html',
-    seeSource: 'https://github.com/mkaya13/leaderboard-project',
+    text: 'A platform for movies/TV shows, where users can like, look at the details and comments on their favourite movie shows. Furthermore, users are able to see other detailed information such as imdb rating, duration of the shows, genres, language, etc of the movies.',
+    img: 'awesome-series.png',
+    languages: ['html', 'css', 'js', 'api'],
+    seeLive: 'https://mkaya13.github.io/movie_app/dist/index.html',
+    seeSource: 'https://github.com/mkaya13/movie_app',
   },
   {
     title: 'SmartBuild',
@@ -30,7 +30,7 @@ const cardContainer = [
     SubTitle: ['MICROVERSE', 'Web Development', 2022],
     text: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     img: 'ToDoList.png',
-    languages: ['html', 'css', 'javascript', 'ruby on rails'],
+    languages: ['html', 'css', 'javascript'],
     seeLive: 'https://mkaya13.github.io/todolist-with-webpack/dist/index.html',
     seeSource: 'https://github.com/mkaya13/todolist-with-webpack',
   },
@@ -45,6 +45,212 @@ const cardContainer = [
     seeSource: '',
   },
 ];
+
+// Fill Project Pages
+
+const fillPages = () => {
+  const portfolioProjects = document.querySelector('.portfolio-projects');
+
+  for (let i = 0; i < cardContainer.length; i += 1) {
+    const { img, title, text } = cardContainer[i];
+
+    const techUl = document.createElement('ul');
+    techUl.classList.add('languages-layout');
+
+    for (let j = 0; j < cardContainer[i].languages.length; j += 1) {
+      const tech = document.createElement('li');
+      tech.classList.add('languages-list');
+      tech.textContent = cardContainer[i].languages[j];
+      techUl.appendChild(tech);
+    }
+
+    const subTitleUl = document.createElement('ul');
+    subTitleUl.classList.add('project-content-3');
+
+    let counter = 0;
+    let SubTitleCounter;
+
+    for (let k = 0; k < cardContainer[i].SubTitle.length; k += 1) {
+      const subTitle = document.createElement('li');
+      SubTitleCounter = document.createElement('img');
+      SubTitleCounter.src = 'img/counter.svg';
+
+      subTitle.appendChild(SubTitleCounter);
+      subTitle.textContent = cardContainer[i].SubTitle[k];
+
+      subTitleUl.style = 'margin-left:15px;';
+      SubTitleCounter.style = 'padding-right:10px;';
+
+      if (counter === 0) {
+        subTitle.classList.add('title-element');
+        subTitleUl.appendChild(subTitle);
+        counter += 1;
+      } else {
+        subTitle.classList.add('list-element');
+        subTitleUl.appendChild(SubTitleCounter);
+        subTitleUl.appendChild(subTitle);
+      }
+    }
+
+    let pictureSide;
+    let contentSide;
+    if (i % 2 === 0) {
+      pictureSide = 'left-side-picture';
+      contentSide = 'right-side-content';
+    } else {
+      pictureSide = 'right-side-picture';
+      contentSide = 'left-side-content';
+    }
+    portfolioProjects.innerHTML += `
+    <figure class="portfolio-item">
+    <div class="project-images">
+        <img class=${pictureSide} src="./img/${img}" alt="">
+    </div>
+
+    <figcaption>
+
+        <div class=${contentSide}>
+
+            <br>
+
+            <h2 class="project-content-2">${title}</h2>
+
+            <br>
+
+            ${subTitleUl.outerHTML}
+
+            <p class="project-content-4">${text} </p>
+
+            ${techUl.outerHTML}
+
+            <div class="container">
+
+            </div>
+
+        </div>
+
+    </figcaption>
+
+  </figure>
+    `;
+  }
+};
+
+let p;
+let img;
+let title;
+let seeLive;
+let seeSource;
+let subTitle;
+let text;
+let tech;
+let techUl;
+let subTitleUl;
+
+const fillModals = () => {
+  const container = document.querySelectorAll('.container');
+
+  for (let i = 0; i < cardContainer.length; i += 1) {
+    p = container[i];
+
+    title = document.createElement('h2');
+    title.classList.add('project-content-2');
+    title.textContent = cardContainer[i].title;
+    title = title.outerHTML;
+
+    img = cardContainer[i].img;
+    seeLive = cardContainer[i].seeLive;
+    seeSource = cardContainer[i].seeSource;
+
+    text = document.createElement('p');
+    text.classList.add('project-content-4');
+    text.textContent = cardContainer[i].text;
+    text = text.outerHTML;
+
+    techUl = document.createElement('ul');
+    techUl.classList.add('languages-layout');
+
+    for (let j = 0; j < cardContainer[i].languages.length; j += 1) {
+      tech = document.createElement('li');
+      tech.classList.add('languages-list');
+      tech.textContent = cardContainer[i].languages[j];
+      techUl.appendChild(tech);
+    }
+
+    subTitleUl = document.createElement('ul');
+    subTitleUl.classList.add('project-content-3');
+
+    let counter = 0;
+    let SubTitleCounter;
+
+    for (let k = 0; k < cardContainer[i].SubTitle.length; k += 1) {
+      subTitle = document.createElement('li');
+      SubTitleCounter = document.createElement('img');
+      SubTitleCounter.src = 'img/counter.svg';
+
+      subTitle.appendChild(SubTitleCounter);
+      subTitle.textContent = cardContainer[i].SubTitle[k];
+
+      subTitleUl.style = 'margin-left:15px;';
+      SubTitleCounter.style = 'padding-right:10px;';
+
+      if (counter === 0) {
+        subTitle.classList.add('title-element');
+        subTitleUl.appendChild(subTitle);
+        counter += 1;
+      } else {
+        subTitle.classList.add('list-element');
+        subTitleUl.appendChild(SubTitleCounter);
+        subTitleUl.appendChild(subTitle);
+      }
+    }
+
+    p.innerHTML = `
+        <button type="button" data-bs-toggle="modal" data-bs-target="#myModal-${i}">See Project</button>
+        <div class="modal" style="display:none" id="myModal-${i}">
+          <div class="modal-dialog modal-lg">
+            <div class="modal-content">   
+              <div class="first-popup-content" style="padding:10px;">
+                <div class="modal-header">
+                ${title}
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                ${subTitleUl.outerHTML}
+              </div>
+              <div class="modal-header">
+                <img class="img-popup" src="img/${img}" alt=""> 
+              </div>
+              <div class="modal-header">
+                <div style="padding-left:12px;">
+                  ${text}
+                  ${techUl.outerHTML}
+                </div>
+              </div>
+              <div class="modal-header" style="display:flex; justify-content:center; align-content:center" >
+                <button class="button-class">
+                  <a href=${seeLive} target="_blank">
+                    See Live <img style ="margin-bottom:3px; height:22px; width:22px;"src="img/live-icon.svg" alt="">
+                  </a>
+                </button>
+                <button class="button-class">
+                  <a href=${seeSource} target="_blank">
+                    See Source <img style ="margin-bottom:3px; height:22px; width:22px;"src="img/github-icon.svg" alt="">
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+  }
+};
+
+const fillProjects = async () => {
+  await fillPages();
+  await fillModals();
+};
+
+fillProjects();
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
@@ -64,111 +270,72 @@ document.querySelectorAll('.navbar-links').forEach((n) => {
   });
 });
 
-let p;
-let img;
-let title;
-let seeLive;
-let seeSource;
-let subTitle;
-let text;
-let tech;
-let techUl;
-let subTitleUl;
-const container = document.querySelectorAll('.container');
+// Languages Tech
 
-for (let i = 0; i < cardContainer.length; i += 1) {
-  p = container[i];
+const languages = document.querySelector('.bottom.languages-tech');
+const aboutTech = document.querySelector('.about-technologies');
 
-  title = document.createElement('h2');
-  title.classList.add('project-content-2');
-  title.textContent = cardContainer[i].title;
-  title = title.outerHTML;
+languages.addEventListener('click', (event) => {
+  event.preventDefault();
 
-  img = cardContainer[i].img;
-  seeLive = cardContainer[i].seeLive;
-  seeSource = cardContainer[i].seeSource;
-
-  text = document.createElement('p');
-  text.classList.add('project-content-4');
-  text.textContent = cardContainer[i].text;
-  text = text.outerHTML;
-
-  techUl = document.createElement('ul');
-  techUl.classList.add('languages-layout');
-
-  for (let j = 0; j < cardContainer[i].languages.length; j += 1) {
-    tech = document.createElement('li');
-    tech.classList.add('languages-list');
-    tech.textContent = cardContainer[i].languages[j];
-    techUl.appendChild(tech);
+  if (languages.className === 'bottom languages-tech') {
+    languages.className = 'right';
+    aboutTech.style = 'display:none;';
+  } else {
+    languages.className = 'bottom languages-tech';
+    aboutTech.style = 'display: flex; flex-wrap: wrap; gap: 20px 0;';
   }
+});
 
-  subTitleUl = document.createElement('ul');
-  subTitleUl.classList.add('project-content-3');
+// Frameworks
 
-  let counter = 0;
-  let SubTitleCounter;
+const aboutMePage = document.querySelector('.about-me');
+const frameworks = document.querySelector('.right.frameworks-tech');
+const aboutFrameworks = document.querySelector('.about-frameworks');
+const aboutFrameworksLi = document.querySelectorAll(
+  '.tech.mobile-rectangle.framework',
+);
 
-  for (let k = 0; k < cardContainer[i].SubTitle.length; k += 1) {
-    subTitle = document.createElement('li');
-    SubTitleCounter = document.createElement('img');
-    SubTitleCounter.src = 'img/counter.svg';
+frameworks.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    subTitle.appendChild(SubTitleCounter);
-    subTitle.textContent = cardContainer[i].SubTitle[k];
-
-    subTitleUl.style = 'margin-left:15px;';
-    SubTitleCounter.style = 'padding-right:10px;';
-
-    if (counter === 0) {
-      subTitle.classList.add('title-element');
-      subTitleUl.appendChild(subTitle);
-      counter += 1;
-    } else {
-      subTitle.classList.add('list-element');
-      subTitleUl.appendChild(SubTitleCounter);
-      subTitleUl.appendChild(subTitle);
+  if (frameworks.className === 'right frameworks-tech') {
+    frameworks.className = 'bottom';
+    aboutFrameworks.style = 'display:flex; flex-wrap: wrap; gap: 20px 5px;';
+    aboutMePage.style.minHeight = '130vh';
+    for (let i = 0; i < aboutFrameworksLi.length; i += 1) {
+      aboutFrameworksLi[i].style = 'display:flex;';
     }
+  } else {
+    frameworks.className = 'right frameworks-tech';
+    aboutFrameworks.style = 'display:none';
   }
+});
 
-  p.innerHTML = `
-      <button type="button" data-bs-toggle="modal" data-bs-target="#myModal-${i}">See Project</button>
-      <div class="modal" style="display:none" id="myModal-${i}">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-content">   
-            <div class="first-popup-content" style="padding:10px;">
-              <div class="modal-header">
-              ${title}
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-              </div>
-              ${subTitleUl.outerHTML}
-            </div>
-            <div class="modal-header">
-              <img class="img-popup" src="img/${img}" alt=""> 
-            </div>
-            <div class="modal-header">
-              <div style="padding-left:12px;">
-                ${text}
-                ${techUl.outerHTML}
-              </div>
-            </div>
-            <div class="modal-header" style="display:flex; justify-content:center; align-content:center" >
-              <button class="button-class">
-                <a href=${seeLive} target="_blank">
-                  See Live <img style ="margin-bottom:3px; height:22px; width:22px;"src="img/live-icon.svg" alt="">
-                </a>
-              </button>
-              <button class="button-class">
-                <a href=${seeSource} target="_blank">
-                  See Source <img style ="margin-bottom:3px; height:22px; width:22px;"src="img/github-icon.svg" alt="">
-                </a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-}
+// Tools
+
+const tools = document.querySelector('.right.tools-tech');
+const aboutTools = document.querySelector('.about-tools');
+const aboutToolsLi = document.querySelectorAll(
+  '.tech.mobile-rectangle.tools',
+);
+
+tools.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (tools.className === 'right tools-tech') {
+    tools.className = 'bottom';
+    aboutTools.style = 'display:flex; flex-wrap: wrap; gap: 20px 5px; padding-bottom: 50px;';
+    aboutMePage.style.minHeight = '130vh';
+    for (let i = 0; i < aboutToolsLi.length; i += 1) {
+      aboutToolsLi[i].style = 'display:flex;';
+    }
+  } else {
+    tools.className = 'right tools-tech';
+    aboutTools.style = 'display:none';
+    aboutMePage.style.minHeight = '110vh';
+  }
+});
 
 const form = document.querySelector('#contact-me');
 const EMAIL_INVALID = 'Please enter your email without uppercase';
@@ -234,7 +401,3 @@ textArea.addEventListener('input', (event) => {
   sessionObj.textArea = event.target.value;
   stringifier();
 });
-
-// if (localStorage.test === undefined) {
-//   localStorage.setItem('test', JSON.stringify(sessionObj));
-// }
